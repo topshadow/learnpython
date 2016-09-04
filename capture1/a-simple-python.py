fcdeb6ccac3084268e6ebc976c69e9ff5ee14a8c
topshadow <731502029@qq.com>
2016-09-04 23:31:03 +0800

    a simple python

--- capture1/a-simple-python.py
+++ capture1/a-simple-python.py
@@ -1,14 +1,9 @@
+import numpy as np
 def pythonsum(n):
-        a = range(n)
-        b = range(n)
-        c = []
-
-        for i in range(len(a)):
-            a[i] = i 
-            # b[i] = i ** 3
-            # c.append(a[i] + b[i])
-
-        return c
+    a = np.arange(n) ** 2
+    b = np.arange(n) ** 3
+    c = a+b
+    return c
     
 print(pythonsum(3))
             
